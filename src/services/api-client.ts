19cb7963afaf06032d5f2cfcc8ca923e0c757a50
Abdul Wahab Achakzai <a.wahabachakzai@gmail.com>
2023-10-14 12:55:24 +0430

    create a custom hook to featch games

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,10 @@
+
 import axios from "axios";
 
 export default axios.create({
+
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "dcc2f07e6ad34a7599997421344a911d",
+    key: "a968757dbb4940d7a15b8f7983a0381d",
   },
 });
