408375c73722a594552030c798427f21800a58c0
Abdul Wahab Achakzai <a.wahabachakzai@gmail.com>
2023-10-14 14:18:56 +0430

    add[feat]: Build game cards

--- src/App.tsx
+++ src/App.tsx
@@ -4,25 +4,25 @@ import { GameGrid } from "./components/GameGrid";
 
 function App() {
   return (
-    <Grid
-      templateAreas={{
-        base: `"nav" "main"`,
-        lg: `"nav nav" "aside main"`,
-      }}
-    >
-      <GridItem area="nav">
-        <NavBar />
-      </GridItem>
-      <Show above="lg">
-        <GridItem area="aside" bg="gold">
-          Aside
-        </GridItem>
-      </Show>
-      <GridItem area="main" bg="dodgerblue">
-        <GameGrid />
-      </GridItem>
-    </Grid>
-  );
+		<Grid
+			templateAreas={{
+				base: `"nav" "main"`,
+				lg: `"nav nav" "aside main"`,
+			}}
+		>
+			<GridItem area="nav">
+				<NavBar />
+			</GridItem>
+			<Show above="lg">
+				<GridItem area="aside" bg="gold">
+					Aside
+				</GridItem>
+			</Show>
+			<GridItem area="main">
+				<GameGrid />
+			</GridItem>
+		</Grid>
+	);
 }
 
 export default App;
