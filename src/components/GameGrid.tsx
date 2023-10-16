76ab88012a4bedd5496fdf8107ff1c910a7abfd0
Abdul Wahab Achakzai <a.wahabachakzai@gmail.com>
2023-10-16 16:33:02 +0430

    add[feat]: Create custom data fetching hook

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,12 +1,12 @@
 import { SimpleGrid, Text } from "@chakra-ui/react";
-import useGames from "./../hooks/useGames";
+import useGames, { Game } from "./../hooks/useGames";
 import { GameCard } from "./GameCard";
 import { GameCardSkeleton } from "./GameCardSkeleton";
 import { GameCardContainer } from "./GameCardContainer";
 
 export const GameGrid = () => {
-	const { games, error, isLoading } = useGames();
-	const skeletons = [1, 2, 3, 4, 5, 6];
+	const { data, error, isLoading } = useGames();
+	const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 	return (
 		<>
@@ -22,7 +22,7 @@ export const GameGrid = () => {
 							<GameCardSkeleton key={skeleton} />
 						</GameCardContainer>
 					))}
-				{games.map((game) => (
+				{data.map((game) => (
 					<GameCardContainer>
 						<GameCard key={game.id} game={game}></GameCard>
 					</GameCardContainer>
--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,11 +1,12 @@
-import useGeneres from "../hooks/useGenres";
+import useData from "../hooks/useData";
+import { Genre } from "../hooks/useGenres";
 
 export const GenreList = () => {
-	const { genres } = useGeneres();
+	const { data } = useData<Genre>("/genres");
 
 	return (
 		<ul>
-			{genres.map((genre) => (
+			{data.map((genre) => (
 				<li key={genre.id}>{genre.name}</li>
 			))}
 		</ul>
