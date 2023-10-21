160c1a15d80a536cf840f74df776364e67008f2f
Abdul Wahab Achakzai <a.wahabachakzai@gmail.com>
2023-10-21 19:44:50 +0430

    add[feat]: Build game search input box

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -4,13 +4,13 @@ export const ColorModeSwitch = () => {
   const { toggleColorMode, colorMode } = useColorMode();
 
   return (
-    <HStack>
-      <Switch
-        colorScheme="green"
-        isChecked={colorMode === "dark"}
-        onChange={toggleColorMode}
-      />
-      <Text> Dark Mode </Text>
-    </HStack>
-  );
+		<HStack>
+			<Switch
+				colorScheme="green"
+				isChecked={colorMode === "dark"}
+				onChange={toggleColorMode}
+			/>
+			<Text whiteSpace="nowrap"> Dark Mode </Text>
+		</HStack>
+	);
 };
