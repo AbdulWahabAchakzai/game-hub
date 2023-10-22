0a2f00feae5b082fc3ae7f975f03876bd33d5bc3
Abdul Wahab Achakzai <a.wahabachakzai@gmail.com>
2023-10-22 20:01:53 +0430

    add[feat]: Implement game searching.

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,8 @@ import { SortSelector } from "./components/SortSelector";
 export interface GameQuery {
 	genre: Genre | null;
 	platform: Platform | null;
-	sortOrder: string | null;
+	sortOrder: string;
+	searchText: string;
 }
 
 function App() {
@@ -29,7 +30,9 @@ function App() {
 			}}
 		>
 			<GridItem area="nav">
-				<NavBar />
+				<NavBar
+					onSearch={(searchText) => setGameQuery({ ...gameQuery, searchText })}
+				/>
 			</GridItem>
 			<Show above="lg">
 				<GridItem area="aside" paddingX={5}>
