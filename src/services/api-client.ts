6857feeaec944de071b041de3d394cfa7cdc8bc5
Abdul Wahab Achakzai <abdulwahabachakzai@Wahabs-MacBook-Pro.local>
2023-11-14 18:15:48 +0430

    refac: fetch games with React Query.

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,9 +1,12 @@
 import axios from "axios";
 const RAWGapiKey = import.meta.env.VITE_RAWG_API_KEY;
 
+export interface FetchResponse<T> {
+  count: number;
+  results: T[];
+}
 
 export default axios.create({
-
   baseURL: "https://api.rawg.io/api",
   params: {
     key: RAWGapiKey,
