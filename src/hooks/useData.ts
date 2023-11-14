5eb96bb1414d8d6b93481bf1711bde39c2e809de
Abdul Wahab Achakzai <abdulwahabachakzai@Wahabs-MacBook-Pro.local>
2023-11-14 10:49:47 +0430

    add[feat]: React query client & implement fetch Genres.

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -1,43 +1,45 @@
-
-
-
 import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { AxiosRequestConfig, CanceledError } from "axios";
 
-interface FetchResponse<T> {
-    count: number;
-    results: T[]
+export interface FetchResponse<T> {
+  count: number;
+  results: T[];
 }
 
+const useData = <T>(
+  endPoint: string,
+  requestConfig?: AxiosRequestConfig,
+  deps?: any[]
+) => {
+  const [data, setData] = useState<T[]>([]);
+  const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
 
-const useData = <T>(endPoint: string, requestConfig?: AxiosRequestConfig, deps?: any[]) => {
-    const [data, setData] = useState<T[]>([]);
-    const [error, setError] = useState("");
-    const [isLoading, setLoading] = useState(false);
+  useEffect(
+    () => {
+      const controller = new AbortController();
+      setLoading(true);
 
-    useEffect(() => {
-        const controller = new AbortController();
-        setLoading(true);
+      apiClient
+        .get<FetchResponse<T>>(endPoint, {
+          signal: controller.signal,
+          ...requestConfig,
+        })
+        .then((res) => {
+          setData(res.data.results);
+          setLoading(false);
+        })
+        .catch((err) => {
+          if (err instanceof CanceledError) return;
+          setError(err.message);
+          setLoading(false);
+        });
+    },
+    deps ? [...deps] : []
+  );
 
-        apiClient.get<FetchResponse<T>>(endPoint, { signal: controller.signal, ...requestConfig }).
-            then((res) => {
-                setData(res.data.results);
-                setLoading(false)
-            }).catch((err) => {
-                if (err instanceof CanceledError) return;
-                setError(err.message);
-                setLoading(false)
-            })
-    }, deps ? [...deps] : []);
-
-    return { data, isLoading, error }
-}
+  return { data, isLoading, error };
+};
 
 export default useData;
-
-
-
-
-
-
--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,20 +1,28 @@
-
+import { useQuery } from "@tanstack/react-query";
 import genres from "../data/genres";
+import apiClient from "../services/api-client";
+import { FetchResponse } from "./useData";
 
 export interface Genre {
-    id: number;
-    name: string;
-    image_background: string;
+  id: number;
+  name: string;
+  image_background: string;
 }
 
 // Accessing genres by RAWG API.
 // const useGeneres = () => useData('/genres')
 
 // Accessing genres from static data to enhance application performance.
-const useGeneres = () => ({ data: genres, isLoading: false, error: null })
+// const useGeneres = () => ({ data: genres, isLoading: false, error: null })
 
+// REACT QUERY IMPLEMENTATION
+const useGeneres = () =>
+  useQuery({
+    queryKey: ["genres"],
+    queryFn: () =>
+      apiClient.get<FetchResponse<Genre>>("/genres").then((res) => res.data),
+    staleTime: 24 * 60 * 60 * 1000, // 24 Hours
+    initialData: { count: genres.length, results: genres },
+  });
 
 export default useGeneres;
-
-
-
