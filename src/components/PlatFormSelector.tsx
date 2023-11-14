713b10f8a97b9f147357196e275d9381f5f5bbd9
Abdul Wahab Achakzai <abdulwahabachakzai@Wahabs-MacBook-Pro.local>
2023-11-14 12:22:09 +0430

    add[feat]: implement React Query in usePlatForms.

--- src/components/PlatFormSelector.tsx
+++ src/components/PlatFormSelector.tsx
@@ -4,33 +4,33 @@ import usePlatforms from "../hooks/usePlatforms";
 import { Platform } from "../hooks/useGames";
 
 interface Props {
-	onSelectedPlatform: (platForm: Platform) => void;
-	selectedPlatform: Platform | null;
+  onSelectedPlatform: (platForm: Platform) => void;
+  selectedPlatform: Platform | null;
 }
 
 export const PlatFormSelector = ({
-	selectedPlatform,
-	onSelectedPlatform,
+  selectedPlatform,
+  onSelectedPlatform,
 }: Props) => {
-	const { data, error } = usePlatforms();
+  const { data, error } = usePlatforms();
 
-	if (error) return null;
+  if (error) return null;
 
-	return (
-		<Menu>
-			<MenuButton as={Button} rightIcon={<BsChevronDown />}>
-				{selectedPlatform?.name || "Platforms"}
-			</MenuButton>
-			<MenuList>
-				{data.map((platform) => (
-					<MenuItem
-						onClick={() => onSelectedPlatform(platform)}
-						key={platform.id}
-					>
-						{platform.name}
-					</MenuItem>
-				))}
-			</MenuList>
-		</Menu>
-	);
+  return (
+    <Menu>
+      <MenuButton as={Button} rightIcon={<BsChevronDown />}>
+        {selectedPlatform?.name || "Platforms"}
+      </MenuButton>
+      <MenuList>
+        {data?.results.map((platform) => (
+          <MenuItem
+            onClick={() => onSelectedPlatform(platform)}
+            key={platform.id}
+          >
+            {platform.name}
+          </MenuItem>
+        ))}
+      </MenuList>
+    </Menu>
+  );
 };
