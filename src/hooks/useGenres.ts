3c7632f4b7df18a91bbadd88bd4825af65016941
Abdul Wahab Achakzai <abdulwahabachakzai@Wahabs-MacBook-Pro.local>
2023-11-15 19:23:43 +0430

    add[feat]: infinite query in games.

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,6 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import genres from "../data/genres";
-import APICient, { FetchResponse } from "../services/api-client";
+import APICient from "../services/api-client";
 
 export interface Genre {
   id: number;
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,7 @@ const RAWGapiKey = import.meta.env.VITE_RAWG_API_KEY;
 
 export interface FetchResponse<T> {
   count: number;
+  next: string | null;
   results: T[];
 }
 
