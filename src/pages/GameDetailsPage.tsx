8f48efdf775428daf4a2d6ec47a62cc9a83f394f
Abdul Wahab Achakzai <abdulwahabachakzai@Wahabs-MacBook-Pro.local>
2023-11-30 19:33:28 +0430

    add[feat]: Game screen shots in game details page.

--- src/pages/GameDetailsPage.tsx
+++ src/pages/GameDetailsPage.tsx
@@ -4,6 +4,7 @@ import ExpandableText from "../components/ExpandableText";
 import GameAttributes from "../components/GameAttributes";
 import GameTrailer from "../components/GameTrailer";
 import useGame from "../hooks/useGame";
+import GameScreenShots from "../components/GameScreenShots";
 
 const GameDetailsPage = () => {
   const { slug } = useParams();
@@ -20,6 +21,7 @@ const GameDetailsPage = () => {
       <ExpandableText>{game.description_raw}</ExpandableText>
       <GameAttributes game={game} />
       <GameTrailer gameId={game.id} />
+      <GameScreenShots gameId={game.id} />
     </>
   );
 };
