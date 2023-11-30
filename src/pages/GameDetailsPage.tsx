7051516402991b1853ebad2c5c07dd4a9f6d9960
Abdul Wahab Achakzai <abdulwahabachakzai@Wahabs-MacBook-Pro.local>
2023-11-30 18:26:47 +0430

    add[feat]: Game info attributes in game details page.

--- src/pages/GameDetailsPage.tsx
+++ src/pages/GameDetailsPage.tsx
@@ -1,7 +1,11 @@
 import { useParams } from "react-router-dom";
 import useGame from "../hooks/useGame";
-import { Heading, Spinner, Text } from "@chakra-ui/react";
+import { Heading, SimpleGrid, Spinner, Text } from "@chakra-ui/react";
 import ExpandableText from "../components/ExpandableText";
+import DefinitionItem from "../components/DefinitionItem";
+import { Platform } from "../entities/Platform";
+import { CriticeScore } from "../components/CriticeScore";
+import GameAttributes from "../components/GameAttributes";
 
 const GameDetailsPage = () => {
   const { slug } = useParams();
@@ -16,6 +20,7 @@ const GameDetailsPage = () => {
     <>
       <Heading>{game.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
+      <GameAttributes game={game} />
     </>
   );
 };
